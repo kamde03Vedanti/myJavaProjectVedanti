53831d8b12c1a0936464e5a6649f34b299e944e4
@kamde03Vedanti <vedantikamde25@gmail.com>
2024-01-15 12:27:20 +0530

    [Vedanti]: Changes Made successfully,check it..

--- src/RepositoriesTest/TC_002_Validate_Flipkart.java
+++ src/RepositoriesTest/TC_002_Validate_Flipkart.java
@@ -2,10 +2,11 @@ package RepositoriesTest;
 
 public class TC_002_Validate_Flipkart {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		
 		System.out.println("TC Flipkart is successfully completed... ");
-
+		System.out.println("Changes is done..with appropriated corrections");
 	}
 
 }
