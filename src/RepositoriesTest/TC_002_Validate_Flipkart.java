14fe5097ef5291df85d17321de07baea194d6d90
@kamde03Vedanti <vedantikamde25@gmail.com>
2024-01-15 12:14:40 +0530

    [Vedanti]: Again Made new Changes in java project

---
+++ src/RepositoriesTest/TC_002_Validate_Flipkart.java
@@ -0,0 +1,11 @@
+package RepositoriesTest;
+
+public class TC_002_Validate_Flipkart {
+
+	public static void main(String[] args) {
+		
+		System.out.println("TC Flipkart is successfully completed... ");
+
+	}
+
+}
